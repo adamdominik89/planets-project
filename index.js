8c2605ec6102e03e9b6789565f7e643705f07771
adamdominik89 <adam.dominik89@gmail.com>
2022-06-29 12:00:33 +0200

    reading file

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-const {parse} = require('csv-parse');
+const { parse } = require('csv-parse');
+const fs = require('fs');
+
+const result = [];
+
+fs.createReadStream('./kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        result.push(data);
+    })
+    .on('error', (error) => {
+        console.error(error);
+    })
+    .on('end', (res) => {
+        console.log(result);
+        console.log('done');
+    });
+
